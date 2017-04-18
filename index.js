e566f83d09c6357461b236257d963d98f744f0c3
James Lemire <webdev.jameslemire@gmail.com>
2017-04-18 09:39:54 -0600

    JL solution - Issue #9.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(express.static( __dirname + '/../' ));
+app.use(express.static( 'public' ));
 app.use(bodyParser.json());
 
 const user = {
